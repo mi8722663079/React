b3e55f40efbe4229482149ce53416a4cfb5a63b6
mi8722663079 <mi8722663@gmail.com>
2023-10-14 09:36:40 +0300

    push

--- my-apps/src/App.jsx
+++ my-apps/src/App.jsx
@@ -1,6 +1,53 @@
+import { useState } from "react";
+import "./styles.css"
+
 export default function App (){
+  const [newItem, setNewItem] = useState('');
+  const [todo, setTodo] = useState([]);
+
+  function handleAdd(){
+    setTodo(copy =>{
+      return [...copy, {id: newItem,completed:false}]
+
+    }
+
+    )
+  }
+  function handleDelete(){
+    setTodo(copy =>{
+      return [...copy.slice(0,-1)]
+    }
+
+    )
+  }
+  console.log(todo[0])
   return(
-    <p>2
-    </p>
+    <>
+<div className="main">
+<div className="wrapper">
+      <h1>Add item</h1>
+      <label htmlFor="search"></label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="search" />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+    <div className="tasks">
+      <h1>TO-DO LIST</h1>
+      <ul>
+        {todo.map( (e) => {
+          return(
+          // eslint-disable-next-line react/jsx-key
+          <li>
+          <label>
+            <input type="checkbox" checked={e.completed} />
+            <p>{e.id}</p>
+          </label>
+          <button className="delete" onClick={handleDelete}>Delete</button>
+        </li>
+          )
+        })}
+      </ul>
+    </div>
+</div>
+</>
   )
 }
