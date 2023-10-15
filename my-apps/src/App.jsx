ac9e630f1a3ac193fd6f5cb36d2e05a0a9869acf
mi8722663079 <mi8722663@gmail.com>
2023-10-15 07:52:36 +0300

    go

--- my-apps/src/App.jsx
+++ my-apps/src/App.jsx
@@ -4,44 +4,50 @@ import "./styles.css"
 export default function App (){
   const [newItem, setNewItem] = useState('');
   const [todo, setTodo] = useState([]);
+  const search = document.querySelector("[type='text']");
 
   function handleAdd(){
-    setTodo(copy =>{
-      return [...copy, {id: newItem,completed:false}]
-
-    }
-
+    setTodo(todos =>{
+        return [...todos, {id: newItem}]
+      }
     )
+    setNewItem("")
   }
-  function handleDelete(){
-    setTodo(copy =>{
-      return [...copy.slice(0,-1)]
-    }
 
-    )
+  function handleDelete(id){
+    return todo.map(dos => {
+      if(dos.id === id){
+        setTodo( todo.filter(dos => dos.id !== id))
+      }
+    })
   }
-  console.log(todo[0])
   return(
     <>
 <div className="main">
 <div className="wrapper">
       <h1>Add item</h1>
       <label htmlFor="search"></label>
-      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="search" />
-      <button onClick={handleAdd}>Add</button>
+      <input
+        type="text" id="search"
+        value={newItem}
+        onKeyDown={e =>{ if(e.target.value === ''){return}  e.key == "Enter"&&handleAdd()}}
+        onChange={e => setNewItem(e.target.value)}
+      />
+      <button onClick={() =>{if(search.value === ''){return} handleAdd()}}>Add</button>
     </div>
     <div className="tasks">
       <h1>TO-DO LIST</h1>
       <ul>
+        {todo.length == 0 &&"No Tasks"}
         {todo.map( (e) => {
           return(
           // eslint-disable-next-line react/jsx-key
-          <li>
+          <li key={e.id}>
           <label>
-            <input type="checkbox" checked={e.completed} />
+            <input type="checkbox" />
             <p>{e.id}</p>
           </label>
-          <button className="delete" onClick={handleDelete}>Delete</button>
+          <button className="delete" onClick={()=> handleDelete(e.id)}>Delete</button>
         </li>
           )
         })}
